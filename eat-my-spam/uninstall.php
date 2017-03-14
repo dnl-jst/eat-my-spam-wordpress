9a3572debd8f72ff49fc1024d4cdda8cdfe65eb8
Daniel Jost <info@daniel-jost.de>
2017-03-14 07:12:03 +0100

    cleanup option in uninstall.php

--- eat-my-spam/uninstall.php
+++ eat-my-spam/uninstall.php
@@ -1,3 +1,10 @@
 <?php
 
 if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') ) exit();
+
+delete_option('eatmyspam_threshold' );
+delete_option('eatmyspam_remove_spam' );
+delete_option('eatmyspam_send_notifications' );
+delete_option('eatmyspam_excluded_rulesets' );
+delete_option('eatmyspam_disable_reports' );
+delete_option('eatmyspam_disable_cf7_integration' );
