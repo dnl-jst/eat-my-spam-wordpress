367ac70e9ec0d67950cd0bf9812fe749a085d6a7
Daniel Jost <info@daniel-jost.de>
2017-03-14 08:04:48 +0100

    coding conventions

--- eat-my-spam/uninstall.php
+++ eat-my-spam/uninstall.php
@@ -1,10 +1,12 @@
 <?php
 
-if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') ) exit();
+if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
 
-delete_option('eatmyspam_threshold' );
-delete_option('eatmyspam_remove_spam' );
-delete_option('eatmyspam_send_notifications' );
-delete_option('eatmyspam_excluded_rulesets' );
-delete_option('eatmyspam_disable_reports' );
-delete_option('eatmyspam_disable_cf7_integration' );
+delete_option( 'eatmyspam_threshold' );
+delete_option( 'eatmyspam_remove_spam' );
+delete_option( 'eatmyspam_send_notifications' );
+delete_option( 'eatmyspam_excluded_rulesets' );
+delete_option( 'eatmyspam_disable_reports' );
+delete_option( 'eatmyspam_disable_cf7_integration' );
